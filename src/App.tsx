184e29eaf40de026715e5e218a158a6f80d4d688
addavriance <61160742+addavriance@users.noreply.github.com>
2024-06-05 01:38:53 +0300

    [0.1.5] Added search feature with query input and results display.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Search from './components/Search';
 
 const App: React.FC = () => {
   return (
       <div className="App">
         <Header />
         <main>
+          <Search />
         </main>
         <Footer />
       </div>
